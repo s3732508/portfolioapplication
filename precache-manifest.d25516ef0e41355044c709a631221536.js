51aa340bd60b2adf4b9b7aa2f7145a0ff6ede5b8
s3732508 <vikasreddy1696@gmail.com>
2020-07-29 14:58:46 +1000

    Updates

---
+++ precache-manifest.d25516ef0e41355044c709a631221536.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe259103a71532351a3da99cee52e70",
+    "url": "/s3732508.github.io/index.html"
+  },
+  {
+    "revision": "4d8f847b2dc33eff273d",
+    "url": "/s3732508.github.io/static/css/main.b8c14270.chunk.css"
+  },
+  {
+    "revision": "88c32755d14fabf4748d",
+    "url": "/s3732508.github.io/static/js/2.f550a38f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/s3732508.github.io/static/js/2.f550a38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8f847b2dc33eff273d",
+    "url": "/s3732508.github.io/static/js/main.bb59e47a.chunk.js"
+  },
+  {
+    "revision": "cd227d17a27e5f90e1e1",
+    "url": "/s3732508.github.io/static/js/runtime-main.1726ba30.js"
+  },
+  {
+    "revision": "dc88d6838a5bab2bc93e03b7ea661bee",
+    "url": "/s3732508.github.io/static/media/Profile.dc88d683.jpg"
+  },
+  {
+    "revision": "dbe096641a49a5335b9720155c6c3dc2",
+    "url": "/s3732508.github.io/static/media/keyboard.dbe09664.jpg"
+  },
+  {
+    "revision": "2590db3a7c52f9290a50c2ea6210618f",
+    "url": "/s3732508.github.io/static/media/opentowork.2590db3a.jpg"
+  },
+  {
+    "revision": "01cf12fbd6abcddc44de3452364f7fc8",
+    "url": "/s3732508.github.io/static/media/sidebarimage.01cf12fb.jpg"
+  },
+  {
+    "revision": "8acfa6376f277a4caf778c46e2cea49e",
+    "url": "/s3732508.github.io/static/media/study.8acfa637.jpg"
+  },
+  {
+    "revision": "a1536a2f685d5672e458d97aa059a87d",
+    "url": "/s3732508.github.io/static/media/worktable.a1536a2f.jpg"
+  }
+]);
